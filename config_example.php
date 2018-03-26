fef77aedf112ec4b4ae2f184dd528fd6bbcc4575
vultr2 <vultr2@vultr.guest>
2018-03-26 10:30:22 +0800

    add config example

---
+++ config_example.php
@@ -0,0 +1,8 @@
+<?php
+
+define("REDIS_HOST", '127.0.0.1');
+define("REDIS_PORT", 6379);
+define("BN_PUBKEY" , "binance-public-key");
+define("BN_SECKEY" , "binance-secret-key");
+define("BN_BASE_URL" , "https://api.binance.com");
+define("BOT_PREFIX", "BOT");
